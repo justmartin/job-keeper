b85cf851f9f0739b229ccf962be084d99142f97e
justmartin <justin.lamar.martin@gmail.com>
2017-04-04 17:17:05 -0700

    Added user_id column to contacts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401072642) do
+ActiveRecord::Schema.define(version: 20170405001633) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170401072642) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "postings", force: :cascade do |t|
